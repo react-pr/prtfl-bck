036b79a1fdc8fd1710eba3c5ef0c08cfb2f7fced
polinom <kutsenko.pn@ucu.edu.ua>
2024-01-22 16:00:52 +0200

    Logic fixing | commit

--- routes/post_routes.py
+++ routes/post_routes.py
@@ -12,9 +12,9 @@ def sendMail(mailer: Mailer):
 	resend.api_key = os.environ["RESEND_API"]
 	params = {
 			"from": "Acme <onboarding@resend.dev>",
-			"to": [f"{mailer.email}"],
+			"to": ["timetotime828@gmail.com"],
 			"subject": "Feedback from portfolio",
-			"html": f"<strong>H!, {mailer.name} ✌️ <br />{mailer.message} <br /><img src='https://www.thedrive.com/uploads/2022/12/13/22C0387_051.jpg?auto=webp&crop=16%3A9&auto=webp&optimize=high&quality=70&width=1440' width='100px' alt='image'></img></strong>",
+			"html": f"<strong>H!, {mailer.name} ✌️ <br />{mailer.message} <br /><img src='https://www.thedrive.com/uploads/2022/12/13/22C0387_051.jpg?auto=webp&crop=16%3A9&auto=webp&optimize=high&quality=70&width=1440' width='100px' alt='image'></img> <br /> <p>From</p>{mailer.email}</strong>",
 	}
 	email = resend.Emails.send(params)
 	return mailer
