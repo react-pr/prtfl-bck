411e392da1dbb2c90902389b186b40181be12c9e
polinom <kutsenko.pn@ucu.edu.ua>
2024-02-02 14:20:19 +0200

    Added changes

--- routes/get_routes.py
+++ routes/get_routes.py
@@ -5,10 +5,6 @@ GET requests
 from appConfig.app import app
 from uiData.data import data
 
-@app.get('/menu_links/')
-def getMenuLinks():
-	return data['menu_links']
-
 @app.get('/projects/')
 def getProjects():
 	return data['projects']
--- uiData/data.py
+++ uiData/data.py
@@ -3,32 +3,6 @@ data config
 """
 
 data = {
-	"menu_links": [
-		{
-			"id": 1,
-			"name": 'Home',
-			"link": '/',
-			"component": 'home',
-		},
-		{
-			"id": 2,
-			"name": 'About',
-			"link": '/about/',
-			"component": 'about',
-		},
-		{
-			"id": 3,
-			"name": 'Projects',
-			"link": '/projects/',
-			"component": 'projects',
-		},
-		{
-			"id": 4,
-			"name": 'Contact',
-			"link": '/contact/',
-			"component": 'contact',
-		},
-	],
 	"projects": [
 		{
 			"id": 1,
